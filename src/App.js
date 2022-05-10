6f27612a263b56dfc8aa2eff9e79e5d6dd90910d
Peach <p22635501@gmail.com>
2022-05-10 23:05:08 +0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Ren,{peach} from './ren';
+import Ren,{peach,Wade} from './ren';
 
 
 function App() {
@@ -8,8 +8,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Ren/>
+        <Ren></Ren>
         <h2>{peach.age}</h2>
+        <Wade id="1" name="wade" age="40"></Wade>
       </header>
     </div>
   );
--- src/ren.js
+++ src/ren.js
@@ -5,24 +5,36 @@ class Ren extends React.Component{
     //render為渲染前最後一個階段
     render(){
       return(
-        <div>
-          helloworld
-        </div>
+        <button>hello,{this.props.name}</button>
       )
     }
 }
 
-//class建構式
+//class component建構式
 class Student{
     constructor(id,name,age){
+        //class的member data，需加上this
         this.id=id
         this.name=name
         this.age=age
     }
 }
 
-
+//class component使用props
+class Wade extends React.Component{
+    constructor(props){
+        super(props)
+        console.log(this.props)
+    }
+    render(){
+        return(
+            <p>{this.props.age}</p>
+        )
+    }
+}
 
 export const peach=new Student(0,"peach",25)
-
+//有定義名稱的class component +{}
+export {Wade}
+//default不用+{}
 export default Ren
